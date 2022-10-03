3a9836ef5227bff37006996932f58d3a42fa9c46
Andrescaba <93167157+Andrescaba@users.noreply.github.com>
2022-10-03 14:38:08 -0300

    Add files via upload

---
+++ js/js2.js
@@ -0,0 +1,7 @@
+function mialerta() {  
+            alert("Esta es una web ilustrativa para mostrar el diseño, creada con Bootstrap por Andres Caballero. Vea la seccion de contacto o visite www.linkedin.com/in/andres-caballero-67829b24a para mas informacion ");
+        } 
+
+function mialerta2() {  
+            alert("Este formulario es ilustrativo y no enviara ningun correo. Para comunicarse utilizar los medios de contacto mencionados al pie de página");
+        } 
